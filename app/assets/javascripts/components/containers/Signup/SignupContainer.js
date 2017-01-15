8e0e14ce15e411c8640b5d6888cb26933fe3bb67
Michael Laythe <mrlaythe24@aol.com>
2017-01-15 13:36:55 -0800

    setup reducers to get ready for signup functionality

---
+++ app/assets/javascripts/components/containers/Signup/SignupContainer.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+class SignupContainer extends Component {
+  render() {
+    return (
+      <div>Signup container</div>
+    );
+  }
+} 
+
+SignupContainer.propTypes = {
+  
+};
+
+export default SignupContainer;
