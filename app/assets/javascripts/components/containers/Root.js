29b6e5d94c3f436b5f290ec8332faa4aa5274246
Michael Laythe <mrlaythe24@aol.com>
2017-01-15 12:05:00 -0800

    render Welcome and get rid of counter example

--- app/assets/javascripts/components/containers/Root.js
+++ app/assets/javascripts/components/containers/Root.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import CounterApp from './CounterApp';
+import Welcome from '../components/Welcome';
 import configureStore from '../store/configureStore';
-import {setCounter} from '../actions/counter'
 
 const store = configureStore();
 
 export default class Root extends Component {
-  componentWillMount() {
-    store.dispatch(setCounter(this.props.counter));
-  }
+
   render() {
     return (
       <Provider store={store}>
-        <CounterApp />
+        <Welcome />
       </Provider>
     );
   }
