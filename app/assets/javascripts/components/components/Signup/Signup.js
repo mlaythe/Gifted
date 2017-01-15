72d2689f97ffe89ecaf077793b58455c112f91f2
Michael Laythe <mrlaythe24@aol.com>
2017-01-15 14:16:43 -0800

    setup skeleton for signup

---
+++ app/assets/javascripts/components/components/Signup/Signup.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+const Signup = () => {
+
+  return (
+    <div>
+      Signup component, yay!
+    </div>
+  );
+};
+
+Signup.propTypes = {
+  
+};
+
+export default Signup;
--- app/assets/javascripts/components/containers/Signup/SignupContainer.js
+++ app/assets/javascripts/components/containers/Signup/SignupContainer.js
@@ -1,4 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Signup from './../../components/Signup/Signup.js';
+import * as signupActions from './../../actions/signupActions.js';
 
 class SignupContainer extends Component {
   render() {
@@ -8,8 +12,21 @@ class SignupContainer extends Component {
   }
 } 
 
+function mapStateToProps(state) {
+  return {
+    userState: state.userState,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    signupActions: bindActionCreators(signupActions, dispatch),
+  };
+}
+
 SignupContainer.propTypes = {
-  
+  signupActions: PropTypes.object,
+  userState: PropTypes.object,
 };
 
-export default SignupContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(SignupContainer);
