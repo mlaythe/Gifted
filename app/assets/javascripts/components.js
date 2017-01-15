255e88a41331362c56f1ee85a4e1f14a9c4b9e76
Michael Laythe <mrlaythe24@aol.com>
2017-01-15 13:33:40 -0800

    add SignupContainer so that it can accessed in DOM

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/containers/Root.js';
+import SignupContainer from './components/containers/Signup/SignupContainer.js';
 
 global.React = React
 global.ReactDOM = ReactDOM
 global.Root = Root
+global.SignupContainer = SignupContainer
