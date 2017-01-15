dd24cba009caafc95684716c923d233cfb660f4b
Michael Laythe <mrlaythe24@aol.com>
2017-01-15 12:04:35 -0800

    remove counter and add Welcome page

---
+++ app/assets/javascripts/components/components/Welcome.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>Welcome!</div>
+    );
+  }
+}
+
+Welcome.propTypes = {
+  
+};
+
+export default Welcome;
