72d1c27515acb57a06e9c3032f087a4790d525c4
Michael Laythe <mrlaythe24@aol.com>
2017-01-16 21:15:13 -0800

    make usernames unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   has_secure_password
 end
